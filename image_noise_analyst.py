8b0fb2c4cc5df3419785300b6deecdab6e463647
X.Y. Hu <thunderlast@gmail.com>
2015-05-07 21:53:05 +0800

    Add Gabor toolkit

--- image_noise_analyst.py
+++ image_noise_analyst.py
@@ -16,6 +16,8 @@ This file can be test standalone using cmd:
 
 import cv2
 import numpy as np
+from math import *
+import matplotlib.pylab as plt
 
 testPath = './test.bmp'
 
@@ -76,6 +78,7 @@ class NoiseAnalyst():
 
     # Public
     img = None 
+    test_winname = "test"
 
     def __init__(self, filename=testPath):
         """
@@ -85,6 +88,9 @@ class NoiseAnalyst():
 
     # ------------------------------------------- Processing
     def showDFT(self):
+        """
+        Return the spectrum in log scale.
+        """
         h, w = self.img.shape[:2]
         realInput = self.img.astype(np.float64)
         # perform an optimally sized dft
@@ -96,8 +102,93 @@ class NoiseAnalyst():
         # no need to pad bottom part of dft_A with zeros because of
         # use of nonzeroRows parameter in cv2.dft()
         cv2.dft(dft_A, dst=dft_A, nonzeroRows=h)
+
+        
+        return dft_A
+
+    def show_sin_wave(self):
+        """
+        Show the origin image with 3 trackbar.
+        These bars control the direction, amplitude and phase 
+        of the compensation sin wave, respectively.
+        A result window shows the result.
+        """
+        h, w = self.img.shape[:2]
+        small_img = cv2.resize(self.img,(w/2,h/2))
+        cv2.imshow(self.test_winname, small_img)
+        cv2.createTrackbar("A", self.test_winname, 
+            0, 100, self.update_sine_win)
+        cv2.createTrackbar("B", self.test_winname, 
+            1, 100, self.update_sine_win)
+        cv2.createTrackbar("amp", self.test_winname, 
+            0, 255, self.update_sine_win)
+        cv2.createTrackbar("pha", self.test_winname, 
+            0, 360, self.update_sine_win)
+        self.update_sine_win()
+        while True:
+            ch = cv2.waitKey() 
+            if ch == 27: # Esc
+                break
+        cv2.destroyWindow(self.test_winname)
+
+    def get_sine_img(self, A, B, amp, pha):
+        """
+        create sine image for compensation.
+        TODO: speed up.
+        """
+        # P(x,y) = Amp * sin( A*x + B*y + Pha)
+        h, w = self.img.shape[:2]
+        sinimg = np.zeros((h, w, 1), dtype=np.uint8)
+        for i in range(h):
+            for j in range(w):
+                sinimg[i,j] = np.uint8(amp * (sin(A*i+B*j+pha)+1))
+        return sinimg
+
+    def get_horizontal_sin_img(self, A, amp, pha):
+        """
+        Get horizontal sine image for compensation.
+        Faster than get_sine_img().
+        """
+        h, w = self.img.shape[:2]
+        tmp = range(h)
+        # create sine wave in 1 col
+        tmp = np.array(range(h), np.float64) 
+        tmp = np.multiply(np.add(np.sin(np.add(np.multiply(tmp,A),pha)),1),amp)
+        tmp = np.uint8(tmp).tolist()
+        # copy to other cols
+        sinimg = np.array([tmp]*w)
+        return sinimg
+        
+
+
+    # ------------------------------------------- Highgui 
+    def show(self, img2show):
+        """
+        Show a image.
+        """
+        cv2.imshow(self.test_winname, img2show)
+        while True:
+            ch = cv2.waitKey() 
+            if ch == 27: # Esc
+                break
+        cv2.destroyWindow(self.test_winname)
+
+    def update_sine_win(self, dummy=None):
+        """
+        Update output image with trackbar.
+        """
+        A = cv2.getTrackbarPos("A", 
+            self.test_winname)
+        B = cv2.getTrackbarPos("B",
+            self.test_winname)
+        amplitude = cv2.getTrackbarPos("amp",
+            self.test_winname)
+        phase = cv2.getTrackbarPos("pha",
+            self.test_winname)
+
+    def show_specturm(self, dft_result):
         # Split fourier into real and imaginary parts
-        image_Re, image_Im = cv2.split(dft_A)
+        image_Re, image_Im = cv2.split(dft_result)
 
         # Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
         magnitude = cv2.sqrt(image_Re**2.0 + image_Im**2.0)
@@ -111,19 +202,16 @@ class NoiseAnalyst():
 
         # normalize and display the results as rgb
         cv2.normalize(log_spectrum, log_spectrum, 0.0, 1.0, cv2.cv.CV_MINMAX)
-        self.show(img2show=log_spectrum)
+        plt.imshow(log_spectrum)
+        plt.show()
+      
 
-    # ------------------------------------------- Highgui 
-    def show(self, img2show, winname="test"):
-        cv2.imshow(winname, img2show)
-        while True:
-            ch = cv2.waitKey() 
-            if ch == 27: # Esc
-                break
-        cv2.destroyWindow(winname)
 
 if __name__ == "__main__":
     na = NoiseAnalyst()
-    na.show(img2show=na.img)
+    # na.show(img2show=na.img)
     na.showDFT()
+    # na.show_sin_wave()
+    # na.get_sine_img(0.03,0,10,10)
+    # na.get_horizontal_sin_img(0.03,100,10)
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 #imageNoiseAnalyst
 
-Using OpenCV-Python to implement GUI program for image noise analysing and help user to find solutions. This program is set as a tool for image processing application in my lab.
+Using OpenCV-Python to develop a toolkit for image noise analysing and help user to find solutions. This program is set as a tool for image processing application in my lab.
 
 ##Target
 1. DFT2 analysing
@@ -12,3 +12,6 @@ Using OpenCV-Python to implement GUI program for image noise analysing and help 
 1. Python 2.7 or higher ( Test with Python 2.7.8 )
 2. python-opencv ( Test with opencv 2.4.4 )
 3. matplotlib ( Test with matplolib 1.4.3)
+
+##Thanks
+1. [dungtn/gaborfun](https://github.com/dungtn/gaborfun)
